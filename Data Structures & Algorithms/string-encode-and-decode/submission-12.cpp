ede87c1320c4a765d21ebecd19be4c8ef64ab43a
Vardaan bajpai <vardaanbajpai492@gmail.com>
2026-08-16 00:29:32 +0530

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s = "";
+
+        for (int i = 0; i < strs.size(); i++) {
+            s += to_string(strs[i].size()) + "#" + strs[i];
+        }
+
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+
+        int i = 0;
+
+        while (i < s.size()) {
+
+            int pos = s.find('#', i);
+
+            int len = stoi(s.substr(i, pos - i));
+
+            ans.push_back(s.substr(pos + 1, len));
+
+            i = pos + 1 + len;
+        }
+
+        return ans;
+    }
+};
