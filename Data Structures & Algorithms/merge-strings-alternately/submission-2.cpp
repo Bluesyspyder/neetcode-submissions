2b3df5e74dbc86061c71d289eecaeb6ae493fae5
Vardaan bajpai <vardaanbajpai492@gmail.com>
2026-08-19 14:56:12 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        string ans="";
+        int i=0,j=0;
+
+        while(i<n && j<m){
+            ans+=word1[i];i++;
+            ans+=word2[j];j++;
+        }
+        while(j<m){
+            ans+=word2[j];j++;
+        }
+        while(i<n){
+            ans+=word1[i];i++;
+        }
+
+        return ans;
+    }
+};
